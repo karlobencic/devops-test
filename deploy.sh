c79e684e514d847df9791fafe34ee1275a47b3ab
Karlo Benčić <karlo.bencic@gmail.com>
2022-07-21 19:46:44 +0200

    setup continuous deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,32 @@
-if [ $# -ne 1 ]; then
-    echo "please specify a build version in the first argument"
+#!/bin/bash
+
+while [[ $# -gt 0 ]] && [[ "$1" == "--"* ]] ;
+do
+    opt="$1";
+    shift;
+    case "$opt" in
+        "--" ) break 2;;
+        "--version_service1" )
+           VERSION_SERVICE1="$1"; shift;;
+        "--version_service1="* )
+           VERSION_SERVICE1="${opt#*=}";;
+        "--version_service2" )
+           VERSION_SERVICE2="$1"; shift;;
+        "--version_service2="* )
+           VERSION_SERVICE2="${opt#*=}";;
+        *) echo >&2 "Invalid option: $@"; exit 1;;
+   esac
+done
+
+if [[ -z $VERSION_SERVICE1 || -z $VERSION_SERVICE2 ]]; then
+    echo "Missing deploy arguments 'version_service1' or 'version_service2'"
     exit 1
 fi
 
-export TAG=$1
+echo "Deploying service 1 using version: ${VERSION_SERVICE1}"
+echo "Deploying service 2 using version: ${VERSION_SERVICE2}"
 
-docker-compose up --build
+export VERSION_SERVICE1
+export VERSION_SERVICE2
+
+docker-compose pull && docker-compose up -d
